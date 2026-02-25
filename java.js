d15ff455c8bfbcb5cedefb10e2221a1eb689e063
unknown <yogeshtiwari0008@gmail.com>
2026-02-25 10:01:53 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+    var num=10;
+    console.log(num);
+    console.log(typeof(num));
+    num=false;
+    console.log(num);
+    console.log(typeof(num));
+    num=undefined;
+    console.log(num);
+    console.log(typeof(num));
